899fa8084eeda918ef06137581175fc09ec636b9
o-h-y-o <khr157929@gmail.com>
2021-11-22 10:43:42 +0900

    ytest

---
+++ src/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {useHistory} from 'react-router-dom';
+
+
+const NotFound = () => {
+  const history = useHistory();
+  return (
+    <div>
+      <h1>주소가 올바르지 않아요!</h1>
+      <button onClick={() => {
+        history.goBack();
+      }}>되돌아가기</button>
+    </div>
+  );
+};
+
+export default NotFound;
